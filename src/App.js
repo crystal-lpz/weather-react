290dbe83f6c3d68b486715c87e78cc18c704ec6a
crystal-lpz <106110698+crystal-lpz@users.noreply.github.com>
2022-09-21 13:31:22 -0700

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <p>
+        <a
+          href="https://github.com/crystal-lpz/weather-react.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code{" "}
+        </a>{" "}
+        by Crystal Lopez{" "}
+      </p>
     </div>
   );
 }
